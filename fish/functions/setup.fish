086a06de9236c3dcdc5815fe0b144ef75537bab3
Scott Takahashi <799718+scott-takahashi@users.noreply.github.com>
2020-05-04 18:54:47 -0700

    Add git abbrs, remove .zshrc

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -4,12 +4,24 @@ function setup
         abbr -e $abbreviation
     end
     abbr g git
-    abbr gf git fetch
-    abbr gs git status
+    abbr ga git commit --amend
+    abbr gac git add -A . && git commit -m
+    abbr ganv git commit --amend --no-verify
     abbr gc git commit -m
-    abbr gp git push
-    abbr gfp git push --force-with-lease
+    abbr gcmp git checkout master && git pull
+    abbr gcnv git commit --no-verify -m
     abbr gd git diff
+    abbr gf git fetch
+    abbr gfp git push --force-with-lease
+    abbr gfpnv git push --force-with-lease --no-verify origin
+    abbr gnb git checkout -b
+    abbr gp git push
+    abbr gpnv git push --no-verify
+    abbr gpub git push -u origin $(git rev-parse --abbrev-ref HEAD)
+    abbr grb git fetch && git rebase origin/master
+    abbr grba git rebase --abort
+    abbr grbc git rebase --continue
+    abbr gs git status
     abbr ll ls -lhA
     fisher
 end
