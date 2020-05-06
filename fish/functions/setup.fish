922eb9224baa595ac5d37285ab5b844bc7d535dc
Scott Takahashi <799718+scott-takahashi@users.noreply.github.com>
2020-05-05 17:01:19 -0700

    Add Figma, Archey, remove invalid fish abbr

--- fish/functions/setup.fish
+++ fish/functions/setup.fish
@@ -5,10 +5,10 @@ function setup
     end
     abbr g git
     abbr ga git commit --amend
-    abbr gac git add -A . && git commit -m
+    # abbr gac git add -A . && git commit -m
     abbr ganv git commit --amend --no-verify
     abbr gc git commit -m
-    abbr gcmp git checkout master && git pull
+    # abbr gcmp git checkout master && git pull
     abbr gcnv git commit --no-verify -m
     abbr gd git diff
     abbr gf git fetch
@@ -17,8 +17,8 @@ function setup
     abbr gnb git checkout -b
     abbr gp git push
     abbr gpnv git push --no-verify
-    abbr gpub git push -u origin $(git rev-parse --abbrev-ref HEAD)
-    abbr grb git fetch && git rebase origin/master
+    abbr gpub git push -u origin (git rev-parse --abbrev-ref HEAD)
+    # abbr grb git fetch && git rebase origin/master
     abbr grba git rebase --abort
     abbr grbc git rebase --continue
     abbr gs git status
