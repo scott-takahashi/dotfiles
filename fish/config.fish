a676376aa06d7bda590176035fe651b39dba578d
Scott Takahashi <799718+scott-takahashi@users.noreply.github.com>
2020-07-20 20:28:50 -0700

    Edit fish prompt, casks, other configs

--- fish/config.fish
+++ fish/config.fish
@@ -16,3 +16,55 @@ set -x -g PATH ~/bin ~/.local/bin $PATH /usr/local/sbin
 # fnm
 set -x -g PATH /home/rosco/.fnm $PATH
 fnm env --multi --use-on-cd | source
+
+# autojump
+[ -f (brew --prefix)/share/autojump/autojump.fish ]; and source (brew --prefix)/share/autojump/autojump.fish
+
+set -g fish_prompt_pwd_dir_length 80
+
+# set normal (set_color normal)
+# set magenta (set_color magenta)
+# set yellow (set_color yellow)
+# set green (set_color green)
+# set red (set_color red)
+# set gray (set_color -o black)
+
+# Fish git prompt
+# set __fish_git_prompt_show_informative_status
+# set __fish_git_prompt_showcolorhints
+# set __fish_git_prompt_showupstream "informative"
+# set __fish_git_prompt_showdirtystate 'yes'
+# set __fish_git_prompt_showstashstate 'yes'
+# set __fish_git_prompt_showuntrackedfiles 'yes'
+# set __fish_git_prompt_showupstream 'yes'
+# set __fish_git_prompt_color_branch yellow
+# set __fish_git_prompt_color_upstream_ahead green
+# set __fish_git_prompt_color_upstream_behind red
+
+set green (set_color green)
+set magenta (set_color magenta)
+set normal (set_color normal)
+set red (set_color red)
+set yellow (set_color yellow)
+
+set __fish_git_prompt_color_branch magenta --bold
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_cleanstate ' 👍 '
+set __fish_git_prompt_char_conflictedstate ' ⚠️ '
+set __fish_git_prompt_char_dirtystate ' 💩 '
+set __fish_git_prompt_char_invalidstate ' 🤮 '
+set __fish_git_prompt_char_stagedstate ' 🚥 '
+set __fish_git_prompt_char_stashstate ' 📦 '
+set __fish_git_prompt_char_stateseparator ' |'
+set __fish_git_prompt_char_untrackedfiles ' 🔍 '
+set __fish_git_prompt_char_upstream_ahead ' ☝️ '
+set __fish_git_prompt_char_upstream_behind ' 👇 '
+set __fish_git_prompt_char_upstream_diverged ' 🚧 '
+set __fish_git_prompt_char_upstream_equal ' 💯 '
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,91 +1,12 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l last_status $status
+    if not set -q prompt_emoji
+        set -g prompt_emoji 👽 👻 🤖 💩 😻 😼 😽 🙌 👏 👍 ✊ ✌️ 🐶 🐱 🐭 🐹 🐰 🦊 🐻 🐼 🐨 🐯 🦁 🐮 🐷 🐽 🐸 🐵 🙈 🙉 🙊 🐒 🐔 🐧 🐦 🐤 🐣 🐥 🦄 🐝 🐛 🦋 🐌 🐚 🐞 🐙 🦑 🦐 🦀 🐡 🐠 🐟 🐬 🐳 🐋 🦈 🐉 🐲 🌵 🎄 🌲 🌳 🌴 🌱 🌿 ☘️ 🍀 🎍 🎋 🍃 🍂 🍁 🍄 🌾 💐 🌷 🌹 🥀 🌺 🌸 🌼 🌻 💫 ⭐️ 🌟 ✨ ⚡️ ☄️ 💥 🔥 🌈 🌊 🍏 🍎 🍐 🍊 🍋 🍌 🍉 🍇 🍓 🍈 🍒 🍑 🍍 🥥 🥝 🍅 🍆 🥑 🥦 🥒 🌶 🌽 🥕 🥔 🍠 🥐 🍞 🥖 🥨 🧀 🥚 🍳 🥞 🥓 🥩 🍗 🍖 🌭 🍔 🍟 🍕 🥪 🥙 🌮 🌯 🥗 🥘 🥫 🍝 🍜 🍲 🍛 🍣 🍱 🥟 🍤 🍙 🍚 🍘 🍥 🥠 🍢 🍡 🍧 🍨 🍦 🥧 🍰 🎂 🍮 🍭 🍬 🍫 🍿 🍩 🍪 🌰 🥜 🍯 🥛 🍼 ☕️ 🍵 🥤 🍶 🍺 🍻 🥂 🍷 🥃 🍸 🍹 🍾 🎹 🥁 🎷 🎺 🎸 🚀 🛸
+    end
+    set -l chosen_emoji_index (random 0 1 (count $prompt_emoji - 1))
+    set -l chosen_emoji $prompt_emoji[$chosen_emoji_index]
 
-	# Just calculate this once, to save a few cycles when displaying the prompt
-	if not set -q __fish_prompt_hostname
-		set -g __fish_prompt_hostname (hostname|cut -d . -f 1)
-	end
-
-	set -l normal (set_color normal)
-
-	# Hack; fish_config only copies the fish_prompt function (see #736)
-	if not set -q -g __fish_classic_git_functions_defined
-		set -g __fish_classic_git_functions_defined
-
-		function __fish_repaint_user --on-variable fish_color_user --description "Event handler, repaint when fish_color_user changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_host --on-variable fish_color_host --description "Event handler, repaint when fish_color_host changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_status --on-variable fish_color_status --description "Event handler; repaint when fish_color_status changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		function __fish_repaint_bind_mode --on-variable fish_key_bindings --description "Event handler; repaint when fish_key_bindings changes"
-			if status --is-interactive
-				commandline -f repaint ^/dev/null
-			end
-		end
-
-		# initialize our new variables
-		if not set -q __fish_classic_git_prompt_initialized
-			set -qU fish_color_user; or set -U fish_color_user -o green
-			set -qU fish_color_host; or set -U fish_color_host -o cyan
-			set -qU fish_color_status; or set -U fish_color_status red
-			set -U __fish_classic_git_prompt_initialized
-		end
-	end
-
-	set -l color_cwd
-	set -l prefix
-	switch $USER
-	case root toor
-		if set -q fish_color_cwd_root
-			set color_cwd $fish_color_cwd_root
-		else
-			set color_cwd $fish_color_cwd
-		end
-		set suffix '#'
-	case '*'
-		set color_cwd $fish_color_cwd
-		set suffix '>'
-	end
-
-	set -l prompt_status
-	if test $last_status -ne 0
-		set prompt_status ' ' (set_color $fish_color_status) "[$last_status]" "$normal"
-	end
-
-	set -l mode_str
-	switch "$fish_key_bindings"
-	case '*_vi_*' '*_vi'
-		# possibly fish_vi_key_bindings, or custom key bindings
-		# that includes the name "vi"
-		set mode_str (
-			echo -n " "
-			switch $fish_bind_mode
-			case default
-				set_color --bold --background red white
-				echo -n "[N]"
-			case insert
-				set_color --bold green
-				echo -n "[I]"
-			case visual
-				set_color --bold magenta
-				echo -n "[V]"
-			end
-			set_color normal
-		)
-	end
-
-	echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $fish_color_host) "$__fish_prompt_hostname" $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal (__fish_git_prompt) $normal $prompt_status "$mode_str" "> "
+    # if test $fish_prompt_pwd_dir_length -eq 0
+    # end
+    echo -s (set_color $fish_color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt)
+    echo -n -s $chosen_emoji ' '
 end
